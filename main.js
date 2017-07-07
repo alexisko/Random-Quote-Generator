2bc90b7ad06fd7b3a52e0b48dccdd4f3bafaf6ef
Alexis <alexisko@hawaii.edu>
2017-07-07 00:01:54 -1000

    Working on CSS

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 console.log("sanity check");
-var quotes = ["move, bitch.", "It's not just a bag.<br>It's prada.", "I'm so frick-in' pissed!",
+var quotes = ["Move, bitch.", "It's not just a bag.<br>It's prada.", "I'm so frick-in' pissed!",
 "Yo, hold my poodle.<br>Hold my poodle!", "Hi, I'm Cellulite Sally!<br>Look at my huge ba-donkey!",
-"She's having a bitch fit!", "Tiple t, k, a.<br>Time to totally kick ass!", "For the lady, perhaps a salad?<br>Perhaps not!",
+"She's having a bitch fit!", "Triple t, k, a.<br>Time to totally kick ass!", "For the lady, perhaps a salad?<br>Perhaps not!",
 "Squeeze me!", "It's the Beverly Ho-Billies.", "Sombody throw Shamu back in the ocean.<br>Whoo!", "Don't hate me 'cause you ain't me!"];
 
 var colors;
 var randomNum, lastNum;
 
 var quote = document.getElementById('quote');
-var btn = document.getElementById('new-quote');
+var btn = document.getElementById('new-quote-btn');
+
 //
 function getRandomNum() {
   return Math.floor(Math.random() * (quotes.length-1 + 1));
